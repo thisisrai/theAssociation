b78e7320de5ccf94962df7a2a17e3241c586321b
Rai Lee <thisisrailee@gmail.com>
2018-05-18 15:04:48 -0700

    player post route

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,16 @@
 class PlayersController < ApplicationController
   def index
     @players = Player.all
-  end 
+  end
+  def create
+    player = Player.new
+    player.first_name = params[:player][:first_name]
+    player.last_name = params[:player][:last_name]
+    if player.save
+      redirect_to "/players"
+    else
+      flash[:errors] = player.errors.full_messages
+      redirect_to "/players/new"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "players" => "players#index"
   get "players/new" => "players#new"
+  post "players" => "players#create"
 end
